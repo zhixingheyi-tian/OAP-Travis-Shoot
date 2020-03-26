eb6d8958e45da79f61a5134c3b767238ead4a72b
zhixingheyi-tian <41657774+zhixingheyi-tian@users.noreply.github.com>
2020-03-26 10:59:47 +0800

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -ex
-	sudo apt-get install cmake
-	sudo apt-get install libpthread-stubs0-dev
-	sudo apt-get install libnuma-dev
+	apt-get install cmake
+	apt-get install libpthread-stubs0-dev
+	apt-get install libnuma-dev
 
 
 
